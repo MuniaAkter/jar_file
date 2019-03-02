0238050001809170399091aa6b8cb95a62fa6d7b
Munia Akter <muniaakter@munias-mbp.lan>
2019-03-02 12:56:28 -0500

    git add

---
+++ src/jar_file/Practice.java
@@ -0,0 +1,33 @@
+package jar_file;
+
+public class Practice {
+	int a= 20;
+	int b= 40;
+	byte c= 50;
+	int d= 190;
+	String o= "67";
+	
+	void pa() {
+		System.out.println(a*b*c);
+		System.out.println(d/c);
+		System.out.println(c*b);
+		String j=o+c;
+			
+	}
+	void ma() {
+		System.out.println(d/c);
+		System.out.println(c*b);
+	}
+	
+	
+	
+	
+	
+	
+	
+
+	
+	
+	
+
+}
---
+++ src/jar_file/Practice2.java
@@ -0,0 +1,30 @@
+package jar_file;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Practice2 {
+
+	public static void main(String[] args) {
+		Practice zzz = new Practice ();
+		zzz.pa();
+		zzz.ma();
+		
+		
+		
+		
+				
+		System.setProperty("webdriver.chrome.driver","/Users/muniaakter/Downloads/chromedriver");
+
+		ChromeDriver dr = new ChromeDriver();
+
+		dr.get("https://www.facebook.com/");
+
+		
+	
+
+	
+		
+
+	}
+
+}
